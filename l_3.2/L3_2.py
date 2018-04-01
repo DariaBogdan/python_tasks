ea06784a8e306f0ee76fc1d871d7d6c1e0037ae3
Daria <a@a.mail.ru>
2018-04-01 17:50:16 +0300

    added

---
+++ l_3.2/L3_2.py
@@ -0,0 +1,20 @@
+import time
+import functools
+
+def decorator(func):
+    '''
+    Calculates and displays the program execution time.
+
+    :param func: time of execution of this function is calculated
+    :type func: function
+    :return: function
+    '''
+
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        t0 = time.time()
+        res = func(*args, **kwargs)
+        t1 = time.time()
+        print(f'Time of execution of this function: {t1 - t0}')
+        return res
+    return inner
