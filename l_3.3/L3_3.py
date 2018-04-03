73116c73101e88184e657f380ae4d9caae892a92
Daria <a@a.mail.ru>
2018-04-03 22:11:05 +0300

    changed

--- l_3.3/L3_3.py
+++ l_3.3/L3_3.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python3.6
 import functools
 
 def validate(low_bound: int, upper_bound: int):
     """Calls function, if its parameters in bounds; else print message.
 
-    :param low_bound: func parameters should be greater or equal to it
+    :param low_bound: func parameters should be greater or equal to it.
     :type low_bound: int
-    :param upper_bound: func parameters should be less or equal to it
+    :param upper_bound: func parameters should be less or equal to it.
     :type upper_bound: int
     :return: function
     """
@@ -18,25 +19,40 @@ def validate(low_bound: int, upper_bound: int):
             if in_bounds:
                 return func(*args, **kwargs)
             else:
-                print('Function call is not valid')
+                print('Function call is not valid.')
         return inner
     return decorator
 
 
 @validate(low_bound=0, upper_bound=256)
 def set_pixel(pixel_values):
+    """Says that pixel created.
+
+    :param pixel_values: amount of pixels at each of three dimensions.
+    :type pixel_values: tuple of three int.
+    :return: None
+    """
     print("Pixel created!")
 
 
-# test
 def another_set_pixel(pixel_values):
+    """Function to test using decorators without using symbol @.
+
+    :param pixel_values: amount of pixels at each of three dimensions.
+    :type pixel_values: tuple of three int.
+    :return: None
+    """
     print("Pixel created!")
 
-deco = validate(low_bound=0, upper_bound=256)
-another_set_pixel = deco(another_set_pixel)
 
+if __name__ == '__main__':
+    deco = validate(low_bound=0, upper_bound=256)
+    another_set_pixel = deco(another_set_pixel)
 
-set_pixel((0,0,0))
-another_set_pixel((0,0,0))
-set_pixel((300,0,0))
-another_set_pixel((300,0,0))
+    # should be equal
+    set_pixel((0,0,0))
+    another_set_pixel((0,0,0))
+
+    #should be equal
+    set_pixel((300,0,0))
+    another_set_pixel((300,0,0))
