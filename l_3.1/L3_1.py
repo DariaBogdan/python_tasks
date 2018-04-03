a73011440c803114e64bc3fbe99a7c4ed803da81
Daria <a@a.mail.ru>
2018-04-03 21:19:29 +0300

    changed

--- l_3.1/L3_1.py
+++ l_3.1/L3_1.py
@@ -1,22 +1,38 @@
-def str_to_num_rec(s: str):
-    """Recursive replaces each character in string with it's ASCII code
+#!/usr/bin/env python3.6
+def digits_count(number: int):
+    """Calculate digits amount of entered number
+    by consistently dividing it by 10.
 
-    :param s: string to transform
-    :return: None if string is empty, else int
+    :param number: integer number.
+    :return: int.
     """
-    if len(s) == 0:
+    digits_amount = 1
+    while number // 10:
+        digits_amount += 1
+        number = number // 10
+    return digits_amount
+
+
+def to_int(string: str) -> int:
+    """Recursive replaces each character in string
+    with it's ASCII code.
+
+    :param string: string to transform.
+    :type string: str.
+    :return: None if string is empty, else int.
+    """
+    if len(string) == 0:
         return None
 
-    if len(s) == 1:
-        return ord(s)
+    if len(string) == 1:
+        return ord(string)
 
-    cur_res = ord(s[-1])
+    # I had to divide return row  into parts
+    shift = 10 ** digits_count(ord(string[-1]))
+    # result of remaining string shifted on len of last ord + last ord
+    return to_int(string[: -1]) * shift + ord(string[-1])
 
-    # calculate length of ord of last element
-    len_cur_res = 1
-    while cur_res // 10:
-        len_cur_res += 1
-        cur_res = cur_res // 10
 
-    # last ord + result of remaining string shifted on len of last ord
-    return str_to_num_rec(s[:-1]) * (10 ** len_cur_res) + ord(s[-1])
+if __name__ == '__main__':
+    print(to_int('abcd'))
+    print(to_int(''))
