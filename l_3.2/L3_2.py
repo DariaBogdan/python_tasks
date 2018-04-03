e916325af4726d2c41e80f1b8e7c624874fedea4
Daria <a@a.mail.ru>
2018-04-03 21:29:39 +0300

    changed

--- l_3.2/L3_2.py
+++ l_3.2/L3_2.py
@@ -1,18 +1,28 @@
+#!/usr/bin/env python3.6
 import time
 import functools
 
 def decorator(func):
     """Calculates and displays the program execution time.
 
-    :param func: time of execution of this function is calculated
-    :type func: function
-    :return: function
+    :param func: time of execution of this function is calculated.
+    :type func: function.
+    :return: function.
     """
     @functools.wraps(func)
     def inner(*args, **kwargs):
-        t0 = time.time()
-        res = func(*args, **kwargs)
-        t1 = time.time()
-        print(f'Time of execution of this function: {t1 - t0}')
-        return res
+        start_time = time.time()
+        func_result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f'Time of execution of function "{func.__name__}": {end_time - start_time}')
+        return func_result
     return inner
+
+
+@decorator
+def test_function(time_to_sleep):
+    time.sleep(time_to_sleep)
+
+
+if __name__ == '__main__':
+    test_function(1)
